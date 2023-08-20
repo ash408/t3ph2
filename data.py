dfca3b7004c17c4742866ee565dbbf954fa98a4a
ash408 <124532093+ash408@users.noreply.github.com>
2023-08-20 13:44:47 -0500

    Added docstring template

--- data.py
+++ data.py
@@ -4,9 +4,32 @@ import pymongo
 from pymongo import MongoClient
 
 class DB():
+	'''
+	Database handler, using pymongo
+
+	Attributes
+	==========
+	variable : type
+		description
+
+	Methods
+	=======
+	TODO(parameters)
+		Description
+		returns ...
+	'''
 
 	def __init__(self):
 		pass
 
 	def TODO(self):
+		'''
+		Description
+
+			Parameters:
+				name (type) : description
+
+			Returns:
+				name (type) : description
+		'''
 		pass
