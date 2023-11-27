0369fd8d41cdfae8eeb0f10f0e86031b79b72c27
ash408 <124532093+ash408@users.noreply.github.com>
2023-11-26 19:37:29 -0500

    Added uid to entity

--- entity.py
+++ entity.py
@@ -1,7 +1,17 @@
 
+import uuid
+
+
 class entity:
 
-	def __init__(self, attributes=None, labels=None, resources=None):
+	def __init__(self, uid=None attributes=None, labels=None, resources=None):
+		
+		if (uid is None):
+			uid = str(uuid.uuid4())
+		
+		self.uid = uid
 		self.attributes = attributes
 		self.labels = labels
 		self.resources = resources
+
+
